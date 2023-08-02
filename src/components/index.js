5bd87fe082b7d69cbceab7459a9cc8f498aeb00f
Om-Aditya-Jain <0808io201033.ies@ipsacademy.org>
2023-08-02 11:54:50 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Carousel } from "./carousel/Carousel";
+export { default as CircleRating } from "./circleRating/CircleRating";
+export { default as ContentWrapper } from "./contentWrapper/ContentWrapper";
+export { default as Footer } from "./footer/Footer";
+export { default as Genres } from "./genres/Genres";
+export { default as Header } from "./header/Header";
+export { default as Img } from "./lazyLoadImage/Img";
+export { default as MovieCard } from "./movieCard/MovieCard";
+export { default as Spinner } from "./spinner/Spinner";
+export { default as SwitchTabs } from "./switchTabs/SwitchTabs";
+export { default as VideoPopup } from "./videoPopup/VideoPopup";
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./home/Home";
+export { default as Details } from "./details/Details";
+export { default as SearchResult } from "./searchResult/SearchResult";
+export { default as Explore } from "./explore/Explore";
+export { default as PageNotFound } from "./404/PageNotFound";
